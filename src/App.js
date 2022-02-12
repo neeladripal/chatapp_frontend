bf14b4f60591a42dad5d113f531bc3daa0172d8e
Neeladri Pal <neel.pal26@gmail.com>
2022-02-13 00:19:50 +0530

    Convert App.js to ffc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Main from "./components/Main";
 import User from "./components/User";
 import auth from "./services/authService";
 import "./App.css";
 
-class App extends Component {
-  state = { user: null };
-
-  componentDidMount() {
-    const user = auth.getCurrentUser();
-    this.setState({ user });
-  }
-
-  render() {
-    const { user } = this.state;
-    return <div className="App">{user ? <Main user={user} /> : <User />}</div>;
-  }
+function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const newUser = auth.getCurrentUser();
+    if (newUser) setUser(newUser);
+  }, []);
+  return <div className="App">{user ? <Main user={user} /> : <User />}</div>;
 }
 
 export default App;
