623872f0e0a516a4b3cf3ee46adfd178e980b353
Neeladri Pal <neel.pal26@gmail.com>
2022-02-21 19:19:03 +0530

    Remove on any event listener in socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,8 +3,4 @@ import config from "../config.json";
 
 const socket = io(config.serverUrl, { autoConnect: false });
 
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
-
 export default socket;
